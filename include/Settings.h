0926d1b6e03a9d5f5b47aa79d7b6917bccd0d8a2
Tanguy Dubois <tanguy.dubois@ls2n.fr>
2025-09-09 13:34:34 +0200

    Settings addresses

---
+++ include/Settings.h
@@ -0,0 +1,11 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+// General settings
+#define SETTING_PAGE 0
+
+// Sequencer page
+#define SETTING_SEQUENCER_PAGE_CHANNELS_BEGIN 1
+#define SETTING_SEQUENCER_PAGE_CHANNELS_END 33
+
+#endif
